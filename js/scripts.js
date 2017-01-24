d7662dbe4c5aa638ed0da3e249f850e5dbf8ca8c
Benjamin T. Seaver and Carlos Munoz Kampff <carlos@>
2017-01-24 11:24:52 -0800

    add html form and submit logic to meet spec 1.

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,11 @@
 // user interface logic
 $(document).ready(function() {
   $("form").submit(function(event) {
+    event.preventDefault();
+    var customerName = $("input#customerName").val();
+    console.log(customerName);
+    var openingBalance = $("input#openingBalance").val();
+    console.log(openingBalance);
+    $("input#accountBalance").val("$0.00");
   });
 });
